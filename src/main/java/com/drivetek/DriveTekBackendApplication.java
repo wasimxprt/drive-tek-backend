1945982b498351d709dd0b5d9fa66f335245cc6d
Wasim Sayyed <wasimxprt@gmail.com>
2021-03-19 10:11:37 +0530

    First commit

---
+++ src/main/java/com/drivetek/DriveTekBackendApplication.java
@@ -0,0 +1,13 @@
+package com.drivetek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveTekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveTekBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivetek/DriveTekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivetek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveTekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
